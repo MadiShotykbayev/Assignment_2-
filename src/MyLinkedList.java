7dfd21f8816a006ec21c7f83ede4a5b2d9b4391a
MadiShotykbayev <113759250+MadiShotykbayev@users.noreply.github.com>
2023-04-19 17:42:55 +0500

    MyLinkedList

---
+++ src/MyLinkedList.java
@@ -0,0 +1,20 @@
+public class MyLinkedList<T> implements MyList<T> {
+    private class Node{
+        T elem;
+        Node nxt;
+        Node pre;
+        public Node(T element) {
+            this.elem = element;
+            this.nxt = null;
+            this.pre = null;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
